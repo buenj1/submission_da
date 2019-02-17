dff8626e0b9c1e23382a2249caea78cd7f3ffbc5
unknown <jbuen32@gmail.com>
2019-02-16 16:58:19 -0800

    Upload of Design Assignment 1A

--- DesignAssignments/DA1A/da1a/da1a/main.asm
+++ DesignAssignments/DA1A/da1a/da1a/main.asm
@@ -1,25 +1,25 @@
 ;John Patrick Buen 
 ;NSHE: 2000900752
-;Email: buenj1@unlv.nevada.edu
-;Github Address: 
-;Directory:  
+;RebelMail: buenj1@unlv.nevada.edu
+;CPE 301: Design Assignment 1A 
+
 
 .include <m328pdef.inc>
-.SET num = 0x0002			;16-bit multiplicand stored in R25:R24
+.SET num = 0xffff			;16-bit multiplicand
 		
 .ORG 00
-	ldi R22, 1				;8-bit multiplier / counter
-	ldi R24, LOW(num)		;16-bit... 
-	ldi R25, HIGH(num)		;Multiplicand  
+	ldi R22, 0xff				;8-bit multiplier / counter
+	ldi R24, LOW(num)		;low byte of 16-bit multiplicand  
+	ldi R25, HIGH(num)		;high byte of 16-bit multiplicand 
 
 	cpi R22, 0				;check if multiplier is 0
-	breq mul0				;branch to mul0 if equal
+	breq mul0				;branch to mul0 if R22 is 0
 	cpi R22, 1				;check if multiplier is 1
 	breq done 				;branch to done if equal 
 
-	cpi R24, 0				;check if low-byte multiplicand is 0
+	cpi R24, 0				;check if low-byte of multiplicand is 0
 	breq chkR25				;jump to chkR25 to check high-byte
-	jmp initialize			;unconditional jump to initialize 
+	jmp initialize			;jump to initialize 
 
 chkR25: 
 	cpi R25, 0				;check if high-byte multiplicand is 0
@@ -27,43 +27,30 @@ chkR25:
 	
 
 initialize: 
-	dec R22					;not counting/multiplying with 1
+	dec R22					;decrement R22, not counting/multiplying with 1
 	mov R16, R24			;fixed adder for low byte  
 	mov R17, R25			;fixed adder for high byte 
-	
+	ldi R23, 0				;fixed 0 to add with R20 in case of a carry bit when adding R25 
+
 lp1: 
-	add R24, R16 
-	brcs addCarry 
-	add R25, R17
-	brcs addCarry2
-	dec R22 
-	breq done
-	jmp lp1
+	adc R24, R16			;add with carry for low byte of multiplicand 
+	adc R25, R17			;add with carry for high byte of multiplicand 
+	adc R20, R23			;add with carry for R20 in case there is a carry bit when adding the high byte (R25)
+	dec R22					;decrement the multiplier 
+	breq done				;if R22 is 0, branch to done 
+	jmp lp1					;else loop back to lp1
 
-addCarry: 
-	adc R25, R17
-	brcs addCarry2
-	dec R22
-	breq done 
-	jmp lp1
-
-addCarry2: 
-	adc R23, R15 
-	dec R22
-	breq done 
-	jmp lp1
-	
 done: 	
-	movw R19:R18, R25:R24
-	mov R20, R23
-	jmp terminate 
+	movw R19:R18, R25:R24	;store 16-bit multiplicand to R19:R18
+	jmp terminate			;jump to terminate to end the program
 	 
 mul0:
-	ldi R18, 0
-	ldi R19, 0
+	ldi R18, 0				;store 0 in R18 if multiplier or low byte is 0
+	ldi R19, 0				;store 0 in R19 if multiplier or high byte is 0
 
 	
 	 
 terminate: 
-	;program is finished 
+
+	jmp terminate			;infinite loop to terminate to stop executing the program
 	
